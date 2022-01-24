8be7829e1aa6e7bb757b6cb999e86aa8d7b64b35
habbimalik <60130739+habbimalik@users.noreply.github.com>
2022-01-24 19:26:13 +0500

    sell id fix

--- DearMonsterClientApp/client/src/utils/constant.js
+++ DearMonsterClientApp/client/src/utils/constant.js
@@ -15,3 +15,4 @@ export const addressList = {
 export const xapiUrl = 'http://aedd-119-155-21-243.ngrok.io'
 export const lapiUrl = 'http://localhost:4000'
 export const apiUrl = 'https://www.dearmonsters.io'
+// export const apiUrl = 'http://localhost:4000'
